44d73ceb82e827c39105962fbd1f7a4f18e3c73c
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-06-09 01:53:33 +0700

    masih lama

--- app/src/main/java/com/project/recipee/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/project/recipee/ui/home/HomeViewModel.kt
@@ -1,16 +1,6 @@
 package com.project.recipee.ui.home
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.project.recipee.data.network.ApiConfig
-import com.project.recipee.data.network.NetworkRecipe
-import com.project.recipee.data.response.SearchResponse
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+class HomeViewModel {
 
 
+}
