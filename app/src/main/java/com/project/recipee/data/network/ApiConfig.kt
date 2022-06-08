284a48c007c9c7cad9ac221d6bdd8753d6e5ac41
lukmannx <lukmannhakimx@gmail.com>
2022-06-08 21:55:39 +0700

    Commit

--- app/src/main/java/com/project/recipee/data/network/ApiConfig.kt
+++ app/src/main/java/com/project/recipee/data/network/ApiConfig.kt
@@ -5,6 +5,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -29,6 +30,7 @@ class ApiConfig {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .client(okHttpClient)
             .build()
             .create(ApiService::class.java)
