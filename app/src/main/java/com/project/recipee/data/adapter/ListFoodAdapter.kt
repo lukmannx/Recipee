3fc131262eb729b681c2bc6cb8062ca5722dc5f1
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-09-12 14:45:44 +0700

    masih lama

--- app/src/main/java/com/project/recipee/data/adapter/ListFoodAdapter.kt
+++ app/src/main/java/com/project/recipee/data/adapter/ListFoodAdapter.kt
@@ -7,52 +7,28 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.project.recipee.R
 import com.project.recipee.data.response.RandomResponse
 import com.project.recipee.databinding.ItemFoodBinding
 
-class ListFoodAdapter(private var listFood: ArrayList<RandomResponse>) : RecyclerView.Adapter<ListFoodAdapter.MyViewHolder>(){
+class ListFoodAdapter(private var listFood: List<RandomResponse>) :
+    RecyclerView.Adapter<ListFoodAdapter.FoodViewHolder>() {
 
-    fun setData(food: List<RandomResponse>?){
-        if (food == null) return
-        listFood.clear()
-        listFood.addAll(food)
-    }
+    inner class FoodViewHolder(val binding: ItemFoodBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private var onItemClickCallback: OnItemClickCallback? = null
-
-    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
-        this.onItemClickCallback = onItemClickCallback
-    }
-
-    class MyViewHolder(val binding: ItemFoodBinding): RecyclerView.ViewHolder(binding.root)
-
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType : Int) =
-
-        MyViewHolder(
-            ItemFoodBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder =
+        FoodViewHolder(
+            ItemFoodBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         )
 
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val food = listFood[position]
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        val foodList = listFood[position]
         holder.binding.apply {
-            //title
-             //title dari RandomResponse
-            itemName.text = food.title
+            itemName.text = foodList.title
 
-             //photo
-            Glide.with(itemPhoto.context)
-                .load(food.image)
-                .apply {RequestOptions()}
-                .override(500,500)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .priority(Priority.HIGH)
+            Glide.with(itemPhoto)
+                .load(foodList.image)
                 .into(itemPhoto)
-
-            holder.itemView.setOnClickListener{
-                onItemClickCallback?.onItemClicked(food)
-            }
         }
     }
 
---
+++ app/src/main/java/com/project/recipee/ui/home/HomeAdapter.kt
@@ -0,0 +1,6 @@
+package com.project.recipee.ui.home
+
+import android.widget.Adapter
+
+class HomeAdapter(private val homeRecipe: List<Adapter>) {
+}
